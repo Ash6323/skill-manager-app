5d6e07dfabf70764161a912b76f84296edc158ce
Ash6323 <ashwin6323@gmail.com>
2023-05-06 00:54:34 +0530

    Profile Section Completed

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -3,15 +3,40 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 import AvatarImage from '../../res/img_avatar.png';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 interface IProfile {
     userFullName: string,
 }
 
+const adminBaseURL = "https://localhost:7247/api/Admin";
+const employeeBaseURL = "https://localhost:7247/api/Employee";
+
+
 const Navbar: React.FC<IProfile> = ({userFullName}) => {
 
     const [signIn, setSignIn] = useState<string>("Sign In"); 
+    const userProps = JSON.parse(localStorage.getItem("User") || '{}');
+    const [profileImage, setProfileImage] = useState<string>(); 
     const navigate = useNavigate();
+
+    const getUser = () => {
+        const url = userProps.role=="Admin"? adminBaseURL : employeeBaseURL;
+        axios.get(`${url}/${userProps.userId}`).then((response) => 
+        {
+            console.log(response.data.data);
+            setProfileImage(response.data.data.profilePictureUrl);
+        }).catch(error => {
+            if(error.response)
+            {
+                alert(error.response.data.message);
+            }
+            else if (error.request)
+            {
+                alert("Server Inactive or Busy");
+            }
+        });
+    }
     
     useEffect( () => {
         if(localStorage.getItem("User") != null)
@@ -50,7 +75,7 @@ const Navbar: React.FC<IProfile> = ({userFullName}) => {
                     <div className="dropdown text-end">
                         <a href="#" className="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" 
                                     data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src={AvatarImage} alt="mdo" width="32" height="32" className="rounded-circle"></img>
+                            <img src={profileImage? profileImage:AvatarImage } alt="mdo" width="32" height="32" className="rounded-circle"></img>
                         </a>
                         <ul className="dropdown-menu text-small" aria-labelledby="dropdownUser1">
                             <li><a className="dropdown-item" onClick={() => navigate("profile")}>{userFullName}</a></li>
--- src/components/Shared/Profile.tsx
+++ src/components/Shared/Profile.tsx
@@ -76,29 +76,68 @@ const Profile = () => {
                     </button>
                 </div> */}
             </div>
-            <div className="row">
-                <div className="d-flex col justify-content-center">
-                    <h3>Profile</h3>
-                </div>
-            </div>
-            <hr></hr>
-            <div className="container d-flex justify-content-center">
-                <div className="card shadow-2-strong card-registration col-md-10">
-                    <img className="card-img-top card-profile-image mt-2" alt="Profile Image"
-                        src={user.profilePictureUrl? user.profilePictureUrl:AvatarImage }>
-                    </img>
-                    <div className="card-body">
-                        <h5 className="card-title">{user.fullName}</h5>
-                        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+            <div className="container d-flex justify-content-center mt-4">
+                <div className="card shadow-2-strong card-registration col-md-9">
+                    <div className="container row mt-5">
+                        <div className='col-md-6'>
+                            <img className="card-img-top card-profile-image mx-5 col-md-6" alt="Profile Image"
+                                src={user.profilePictureUrl? user.profilePictureUrl:AvatarImage }>
+                            </img>
+                        </div>
+                        <div className="col-md-6 text-start">
+                        <ul className="list-unstyled mt-5">
+                            <li className="mb-2 mb-xl-3 display-28">
+                                <span className="display-26 text-secondary me-2 font-weight-600">Name:</span> {user.fullName}
+                            </li>
+                            <li className="mb-2 mb-xl-3 display-28">
+                                <span className="display-26 text-secondary me-2 font-weight-600">Email:</span> {user.email}
+                            </li>
+                            <li className="mb-2 mb-xl-3 display-28">
+                                <span className="display-26 text-secondary me-2 font-weight-600">Phone:</span> {user.phoneNumber}
+                            </li>
+                            <li className="display-28">
+                                <span className="display-26 text-secondary me-2 font-weight-600">Username:</span> {user.userName}
+                            </li>
+                        </ul>
+                        </div>
                     </div>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">Cras justo odio</li>
-                        <li className="list-group-item">Dapibus ac facilisis in</li>
-                        <li className="list-group-item">Vestibulum at eros</li>
-                    </ul>
-                    <div className="card-body">
-                        <a href="#" className="card-link">Card link</a>
-                        <a href="#" className="card-link">Another link</a>
+                    <hr className='mt-4'></hr>
+                    <div className="container row">
+                        <div className='col-md-6'>
+                            <ul className="list-group list-group-flush text-start mx-3">
+                                <li className="mb-2 display-28 list-group-item">
+                                    <span className="display-26 text-secondary me-2 font-weight-600">DOB:</span> 
+                                        {new Date(user.dateOfBirth).toLocaleDateString()}
+                                </li>
+                                <li className="mb-2 display-28 list-group-item">
+                                    <span className="display-26 text-secondary me-2 font-weight-600">Gender:</span> {user.gender}
+                                </li>
+                                <li className="mb-2 display-28 list-group-item">
+                                    <span className="display-26 text-secondary me-2 font-weight-600">Previous Organisation:</span> 
+                                        {user.previousOrganisation}
+                                </li>
+                                <li className="mb-2 display-28 list-group-item">
+                                    <span className="display-26 text-secondary me-2 font-weight-600">Previous Designation:</span> 
+                                        {user.previousDesignation}
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='col-md-6'>
+                            <ul className="list-group list-group-flush text-start mb-4">
+                                <li className="mb-2 display-28 list-group-item">
+                                    <span className="display-26 text-secondary me-2 font-weight-600">Street:</span> {user.street}
+                                </li>
+                                <li className="mb-2 display-28 list-group-item">
+                                    <span className="display-26 text-secondary me-2 font-weight-600">Town:</span> {user.town}
+                                </li>
+                                <li className="mb-2 display-28 list-group-item">
+                                    <span className="display-26 text-secondary me-2 font-weight-600">City:</span> {user.city}
+                                </li>
+                                <li className="mb-2 display-28 list-group-item">
+                                    <span className="display-26 text-secondary me-2 font-weight-600">Zipcode:</span> {user.zipcode}
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
