f1c13975b4b24b162d21dfc927060f9f8c21f50f
Ash6323 <ashwin6323@gmail.com>
2023-05-07 19:46:36 +0530

    UI Changes

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -4,6 +4,7 @@ import AvatarImage from '../../res/img_avatar.png';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import IXBanner from '../../res/IX-banner.png';
 
 interface IProfile {
     userFullName: string,
@@ -60,39 +61,24 @@ const Navbar: React.FC<IProfile> = ({userFullName}) => {
 
     return (
         <>
-        <svg xmlns="http://www.w3.org/2000/svg" style={{display: "none"}}>
-            <symbol id="bootstrap" viewBox="0 0 118 94">
-                <title>EmployeeSkills</title>
-                <path fillRule="evenodd" clipRule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"></path>
-            </symbol>
-        </svg>
-        <header className="mt-3 pb-3 border-bottom">
-            <div className="container-fluid d-grid gap-3 align-items-center">
-                <div className="d-flex flex-wrap align-items-center justify-content-space-around justify-content-lg-start">
-                    <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-                        <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-                            <use xlinkHref="#bootstrap" /></svg>
+        <header className="mt-3 mb-1 border-bottom">
+            <div className="container-fluid">
+                <div className="d-flex flex-wrap justify-content-between">
+                    <a className="d-flex align-items-center text-dark text-decoration-none">
+                        <img className="ix-banner" src={IXBanner}></img>
                     </a>
 
-                    <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="#" className="nav-link px-2 link-secondary">Overview</a></li>
-                        <li><a href="#" className="nav-link px-2 link-dark">Employees</a></li>
-                        <li><a href="#" className="nav-link px-2 link-dark">Skills</a></li>
+                    <a className="d-block link-dark text-decoration-none dropdown-toggle mx-2" id="dropdownUser1"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src={profileImage? profileImage:AvatarImage } width="32" height="32" className="rounded-circle"></img>
+                    </a>
+                    <ul className="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+                        <li><a className="dropdown-item" onClick={profileNavigator}>
+                            {userFullName}</a>
+                        </li>
+                        <li><hr className="dropdown-divider"></hr></li>
+                        <li><a className="dropdown-item" onClick={HandleSignOut}>{signIn}</a></li>
                     </ul>
-
-                    <div className="dropdown text-end">
-                        <a href="#" className="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src={profileImage? profileImage:AvatarImage } width="32" height="32" className="rounded-circle"></img>
-                        </a>
-                        <ul className="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-                            <li><a className="dropdown-item" onClick={profileNavigator}>
-                                {userFullName}</a>
-                            </li>
-                            <li><hr className="dropdown-divider"></hr></li>
-                            <li><a className="dropdown-item" onClick={HandleSignOut}>{signIn}</a></li>
-                        </ul>
-                    </div>
                 </div>
             </div>
         </header></>
