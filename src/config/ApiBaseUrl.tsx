9af2c4833d50570e2cd28d97ffe96ba34c878569
Ash6323 <ashwin6323@gmail.com>
2023-06-27 14:25:19 +0530

    Shifted to Local

--- src/config/ApiBaseUrl.tsx
+++ src/config/ApiBaseUrl.tsx
@@ -1,1 +1,2 @@
-export default "https://employee-skill-manager2.azurewebsites.net/api/";
+// export default "https://employee-skill-manager2.azurewebsites.net/api/";
+export default "https://localhost:7247/api/";
